9fd544cd47ecfc3f34c61dda747b8f6053acacbb
david aussourd <softwatermermaid@hotmail.fr>
2017-04-16 22:44:36 +0200

    Refactoring, documentation, DataManager return copies

--- TemplateEditor/src/main/java/com/dysperia/templateeditor/DataManager.java
+++ TemplateEditor/src/main/java/com/dysperia/templateeditor/DataManager.java
@@ -5,40 +5,71 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * Class managing the TEMPLATE.DAT and POINTER1.DAT data
+ * @author dysperia
+ */
 public class DataManager {
+	/** TEMPLATE.DAT file */
 	private File templateFile;
+	/** POINTER1.DAT file */
 	private File pointer1File;
+	/** Offsets from POINTER1.DAT */
 	private List<Integer> pointersValue;
+	/** Texts from TEMPLATE.DAT */
 	private List<String> templateTexts;
+	/** Titles of the template.dat texts */
+	private List<String> titles;
 
+	/**
+	 * Constructor
+	 * @param templateFile The TEMPLATE.DAT file
+	 * @param pointer1File The POINTER1.DAT file
+	 */
 	public DataManager(File templateFile, File pointer1File) {
 		this.templateFile = templateFile;
 		this.pointer1File = pointer1File;
 		pointersValue = new ArrayList<>();
 		templateTexts = new ArrayList<>();
+		titles = new ArrayList<>();
 	}
 	
+	/**
+	 * Load the text data using POINTER1.DAT and TEMPLATE.DAT
+	 * @throws IOException
+	 */
 	public void readDataFromFiles() throws IOException {
+		this.readOffsets();
+		this.readTemplateTexts();
+		this.buildTitles();
+	}
+	
+	private void readOffsets() throws IOException {
 		try(DataInputStream pointer1IS = new DataInputStream(new FileInputStream(pointer1File))) {
 			pointer1IS.skipBytes(0x2710);
-			int offset = toLittleEndian(pointer1IS.readInt());
+			int offset = changeEndianness(pointer1IS.readInt());
 			while(offset != 0) {
 				pointersValue.add(offset);
-				offset = toLittleEndian(pointer1IS.readInt());
+				offset = changeEndianness(pointer1IS.readInt());
 			}
+			// Adding end of file offset to compute the last template entry length
+			pointersValue.add((int)templateFile.length());
 		}
-		// Adding end of file offset to compute the last template entry length
-		pointersValue.add((int)templateFile.length());
-		
+	}
+	
+	private void readTemplateTexts() throws IOException {
 		try(DataInputStream templateIS = new DataInputStream(new FileInputStream(templateFile))) {
 			for(int i=0; i<pointersValue.size()-1; i++) {
 				int sizeToRead = pointersValue.get(i+1) - pointersValue.get(i);
 				byte[] bytesArray = new byte[sizeToRead];
 				int byteNumberRead = templateIS.read(bytesArray, 0, sizeToRead);
 				if (byteNumberRead != sizeToRead) {
-					System.out.println("[DataManager] Number of bytes read: "+byteNumberRead+", expected: "+sizeToRead);
+					System.err.println("[DataManager] Number of bytes read: "+byteNumberRead+", expected: "+sizeToRead);
 				}
 				String text = new String(bytesArray, "Cp1252");
 				templateTexts.add(text);
@@ -46,7 +77,12 @@ public class DataManager {
 		}
 	}
 	
-	private int toLittleEndian(int val)
+	/**
+	 * Change the endianness of a 32 bits int
+	 * @param val Int to convert
+	 * @return The converted int
+	 */
+	private int changeEndianness(int val)
 	{
 		int i1 = (val >>  0) & 0xff;
 		int i2 = (val >>  8) & 0xff;
@@ -55,7 +91,68 @@ public class DataManager {
 		return i1 << 24 | i2 << 16 | i3 << 8 | i4 << 0;
 	}
 	
+	/**
+	 * Built the titles from the TEMPLATE.DAT texts
+	 */
+	private void buildTitles() {
+		Pattern charsFollowingTitle = Pattern.compile("#[^\\s]*");
+		for(int i=0; i<templateTexts.size(); i++) {
+			String entry = templateTexts.get(i);
+    	    Matcher match = charsFollowingTitle.matcher(entry);
+    	    if(!match.find()) {
+    	    	System.err.println("[DataManager] Title not found in " + entry);
+    	    }
+    	    titles.add(entry.substring(match.start(), match.end()));
+		}
+	}
+	
+	/**
+	 * Return the list of texts. The list is empty until a call to readDataFromFiles() is done
+	 * @return The list of the texts contained in TEMPLATE.DAT
+	 */
 	public List<String> getTemplateTexts() {
-		return this.templateTexts;
+		List<String> copy = new ArrayList<>(this.templateTexts);
+		Collections.copy(copy, this.templateTexts);
+		return copy;
+	}
+	
+	/**
+	 * Set the new texts to the given list
+	 * @param texts new TEMPLATE.DAT texts
+	 */
+	public void setTemplateTexts(List<String> texts) {
+		this.templateTexts = texts;
+		this.buildTitles();
+	}
+	
+	/**
+	 * Return the list of the texts titles. The list is empty until a call to readDataFromFiles() is done
+	 * @return The list of the titles of the texts contained in TEMPLATE.DAT
+	 */
+	public List<String> getTitles() {
+		List<String> copy = new ArrayList<>(this.titles);
+		Collections.copy(copy, this.titles);
+		return copy;
+	}
+	
+	private void convertNewLineCharacters() {
+		// TODO convert
+	}
+	
+	/**
+	 * Save the texts to the given files
+	 * @param templateFile TEMPLATE.DAT File
+	 * @param pointerFile POINTER1.DAT File
+	 */
+	public void saveEditedTexts(File templateFile, File pointerFile) {
+		this.convertNewLineCharacters();
+		// TODO save
+	}
+	
+	/**
+	 * Save the texts to the files given at this object creation
+	 */
+	public void saveEditedTexts() {
+		this.saveEditedTexts(this.templateFile, this.pointer1File);
 	}
 }
--- TemplateEditor/src/main/java/com/dysperia/templateeditor/MainWindow.java
+++ TemplateEditor/src/main/java/com/dysperia/templateeditor/MainWindow.java
@@ -3,8 +3,6 @@ package com.dysperia.templateeditor;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -26,26 +24,50 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-
+/**
+ * Class of the main window
+ * @author dysperia
+ */
 public class MainWindow extends BorderPane {
+	/** Default TextArea text */
 	private final static String defaultText = "First open the TEMPLATE.DAT directory.\n\n"
     		+ "Both the files POINTER1.DAT and TEMPLATE.DAT \n"
     		+ "must be present in the same directory.";
-	
-	private final ObservableList<String> titles = FXCollections.observableArrayList();
+	/** List of the titles */
+	private final ObservableList<String> titles;
+	/** ListView used to select a text from its titles */
 	private final ListView<String> listView;
+	/** List of TEMPLATE.DAT texts */
 	private List<String> templateTexts;
+	/** TextArea used to display the chosen TEMPLATE.DAT text */
 	private final TextArea textArea;
-	private final BooleanProperty modifiedProperty = new SimpleBooleanProperty(false);
+	///** Used to know if a text has been edited */
+	//private final BooleanProperty modifiedProperty = new SimpleBooleanProperty(false);
+	/** FileChooser used to open a TEMPLATE.DAT file */
 	private final FileChooser fileChooser;
+	/** Stage of this window */
 	private final Stage stage;
+	/** Data manager used to load and save data from TEMPLATE.DAT and POINTER1.DAT */
 	private DataManager dataManager;
 	
+	/**
+	 * Constructor
+	 * @param stage Stage of the main window
+	 */
 	public MainWindow(Stage stage) {
 		fileChooser = new FileChooser();
 		this.stage = stage;
-		
+		textArea = new TextArea(defaultText);
+		titles = FXCollections.observableArrayList();
 		listView = new ListView<>(titles);
+        this.setupListViewAndTextArea();
+        this.setupButtonsAndToolbar();
+	}
+	
+	/**
+	 * Init and setup the ListView and the TextArea
+	 */
+	private void setupListViewAndTextArea() {
         listView.setPrefSize(150, 400);
         listView.setEditable(false);
         listView.setItems(titles);
@@ -54,20 +76,25 @@ public class MainWindow extends BorderPane {
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				textArea.setText(templateTexts.get(newValue.intValue()));
 			}
-		});;
+		});
 		setLeft(listView);
-        
-        textArea = new TextArea(defaultText);
-        textArea.setPrefSize(800, 400);
+		
+        textArea.setPrefSize(650, 400);
         textArea.textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 				templateTexts.set(listView.getSelectionModel().getSelectedIndex(), newValue.toString());
+				//MainWindow.this.modifiedProperty.set(true);
 			}
-		});;
+		});
         setCenter(textArea);
-
-        Button openButton = new Button(null, new ImageView(new Image("icon/open.png",24,24,true,true)));
+	}
+	
+	/**
+	 * Init and setup the buttons and the toolbar
+	 */
+	private void setupButtonsAndToolbar() {
+		Button openButton = new Button(null, new ImageView(new Image("icon/open.png",24,24,true,true)));
         openButton.setTooltip(new Tooltip("open"));
         openButton.setOnAction(e -> {
         	fileChooser.setTitle("Open TEMPLATE.DAT");
@@ -89,7 +116,7 @@ public class MainWindow extends BorderPane {
         
         Button saveButton = new Button(null, new ImageView(new Image("icon/save.png",24,24,true,true)));
         saveButton.setTooltip(new Tooltip("save"));
-        saveButton.disableProperty().bind(modifiedProperty.not());
+        //saveButton.disableProperty().bind(modifiedProperty.not());
         
         Button showVariableButton = new Button(null, new ImageView(new Image("icon/view_variable.png",24,24,true,true)));
         showVariableButton.setTooltip(new Tooltip("show variables"));
@@ -103,11 +130,11 @@ public class MainWindow extends BorderPane {
         setTop(toolbar);
 	}
 	
-	public void resetContent() {
-		titles.clear();
-		textArea.setText(defaultText);
-	}
-	
+	/**
+	 * Show an information alert with the given title and message
+	 * @param title Title of the alert
+	 * @param message Message of the alert
+	 */
 	private void showInfoAlert(String title, String message) {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle(title);
@@ -116,23 +143,20 @@ public class MainWindow extends BorderPane {
 		alert.showAndWait();
 	}
 	
+	/**
+	 * Build the data and update the view
+	 * @param templateFile TEMPLATE.DAT File
+	 * @param pointer1File POINTER1.DAT File
+	 */
 	private void buildDataFromFilesAndUpdateView(File templateFile, File pointer1File) {
 		dataManager = new DataManager(templateFile, pointer1File);
 		try {
 			dataManager.readDataFromFiles();
     		templateTexts = dataManager.getTemplateTexts();
-    		Pattern charsFollowingTitle = Pattern.compile("#[^\\s]*");
-    		for(int i=0; i<templateTexts.size(); i++) {
-    			String entry = templateTexts.get(i);
-        	    Matcher match = charsFollowingTitle.matcher(entry);
-        	    if(!match.find()) {
-        	    	System.out.println("[Main] Title not found in " + entry);
-        	    }
-        	    titles.add(entry.substring(match.start(), match.end()));
-    		}
+    		titles.addAll(dataManager.getTitles());
     		listView.getSelectionModel().selectFirst();
 		} catch (IOException e) {
-			showInfoAlert("Cannot read files", "The file POINTER1.DAT or TEMPLATE.DAT was not found or it was impossible to read the data from thme");
+			showInfoAlert("Cannot read files", "The file POINTER1.DAT or TEMPLATE.DAT was not found or it was impossible to read the data from them");
 			e.printStackTrace();
 		}
 	}
